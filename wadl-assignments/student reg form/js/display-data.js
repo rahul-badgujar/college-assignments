ad3605344e1ab26d88793e41ace29f681cc456ab
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-19 09:31:22 +0530

    added dashbaord

---
+++ wadl-assignments/student reg form/js/display-data.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  getData();
+});
+
+function getData() {
+  let localStorageData = localStorage.getItem("student");
+  let studentObj = JSON.parse(localStorageData);
+  console.log(studentObj);
+  $("#rollno").text(studentObj.rollno);
+  $("#firstName").text(studentObj.firstName);
+  $("#lastName").text(studentObj.lastName);
+  $("#dob").text(studentObj.dob);
+  $("#gender").text(studentObj.gender);
+  $("#email").text(studentObj.email);
+  $("#contactNo").text(studentObj.contactNo);
+  $("#branch").text(studentObj.branch);
+  $("#registrationDate").text(studentObj.registrationDate);
+  $("#parentName").text(studentObj.parentName);
+  $("#parentContactNo").text(studentObj.parentMobileNo);
+}
